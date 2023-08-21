6d88847ca57182227ca26b17acf346e8e81689bc
Aaron <aaronborgi1@gmail.com>
2023-08-21 11:57:06 +0100

    fixed facture type

--- src/app/utils/models/Facture.ts
+++ src/app/utils/models/Facture.ts
@@ -4,7 +4,7 @@ export type Facture = {
   id: number;
   client: Client;
   montant: number;
-  dateFacture: string;
-  description: string[];
+  dateFacture: string[];
+  description: string;
   dateEcheance: string[];
 }
