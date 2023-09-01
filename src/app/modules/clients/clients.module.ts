ce7e510b16406133d9ba83206bb50763418b4718
Aaron <aaronborgi1@gmail.com>
2023-09-01 14:45:50 +0100

    module clean ups

--- src/app/modules/clients/clients.module.ts
+++ src/app/modules/clients/clients.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { ClientCardComponent } from "./components/client-card/client-card.component";
 import { ClientTableComponent } from "./components/client-table/client-table.component";
 
 @NgModule({
-	declarations: [ClientCardComponent, ClientTableComponent],
-	exports: [ClientCardComponent, ClientTableComponent],
+	declarations: [ClientTableComponent],
+	exports: [ClientTableComponent],
 	imports: [CommonModule],
 })
 export class ClientsModule {}
--- src/app/modules/facture/facture.module.ts
+++ src/app/modules/facture/facture.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from "@angular/core";
 import { CommonModule, CurrencyPipe } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
 import { FacturePage } from "./pages/facture/facture.page";
-import { FactureCardComponent } from "./components/facture-card/facture-card.component";
 import { FactureTableComponent } from "./components/facture-table/facture-table.component";
 import { CoreModule } from "@core/core.module";
-import { FilterFactureDialogComponent } from './components/filter-facture-dialog/filter-facture-dialog.component';
+import { FilterFactureDialogComponent } from "./components/filter-facture-dialog/filter-facture-dialog.component";
 
 @NgModule({
-	declarations: [FacturePage, FactureCardComponent, FactureTableComponent, FilterFactureDialogComponent],
-	exports: [FacturePage, FactureCardComponent, FactureTableComponent],
+	declarations: [
+		FacturePage,
+		FactureTableComponent,
+		FilterFactureDialogComponent,
+	],
 	imports: [ReactiveFormsModule, CommonModule, CoreModule, CurrencyPipe],
 })
 export class FactureModule {}
